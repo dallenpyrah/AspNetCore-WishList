e792adacfaebac2ea245c31d492779ad2c2b9b35
Dallen Pyrah <dallenpyrah@gmail.com>
2021-05-22 18:32:39 -0600

    Half done

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item Item)
+        {
+            _context.Items.Add(Item);
+        }
+
+        [HttpDelete]
+
+        public IActionResult Delete(int Id)
+        {
+            var item = _context.Items.FirstOrDefault(item => item.Id == Id);
+            if (item != null)
+            {
+                _context.Items.Remove(item);
+                return View();
+            }
+        }
+    }
+}
